02a674d4f4798f193d0a2887204e3f30d98aced6
king <king@qq.com>
2019-10-14 19:35:13 +0800

    初始化脚本

---
+++ script/init_path.sh
@@ -0,0 +1,28 @@
+
+mkdir -p /mysql/13306/data
+mkdir -p /mysql/23306/data
+mkdir -p /mysql/33306/data
+
+mkdir -p /mysql/13306/conf
+mkdir -p /mysql/23306/conf
+mkdir -p /mysql/33306/conf
+
+
+mkdir -p /mysql/slave/conf
+
+
+# cp /etc/my.cnf /mysql/13306/conf/
+# cp /etc/my.cnf /mysql/23306/conf/
+# cp /etc/my.cnf /mysql/33306/conf/
+
+# vi /mysql/13306/conf/my.cnf
+# vi /mysql/23306/conf/my.cnf
+# vi /mysql/33306/conf/my.cnf
+
+mkdir -p /mysql/common/data
+# cp -fr /mysql/common/data/mysql/* .
+
+# 目录访问权限
+chmod -R 777 /mysql/13306/data
+chmod -R 777 /mysql/23306/data
+chmod -R 777 /mysql/33306/data
